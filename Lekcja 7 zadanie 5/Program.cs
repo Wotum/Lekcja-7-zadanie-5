4cb4d5dd23d4569a4090c1b9a70d2dfc947ccc1c
HARDPC <HARDPC@WOTUM>
2024-01-03 15:39:21 +0100

    Add project files.

---
+++ Lekcja 7 zadanie 5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace CzyMozeszZostacPrezydentem;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Podaj swój wiek:");
+        int wiek;
+
+        if (int.TryParse(Console.ReadLine(), out wiek))
+        {
+            if (wiek >= 35)
+            {
+                Console.WriteLine("Możesz zostać prezydentem");
+            }
+            else
+                Console.WriteLine("Nie możesz zostać prezydentem");
+
+            if(wiek >= 30)
+            {
+                Console.WriteLine("Możesz zostać senatorem");
+            }
+            else
+            {
+                Console.WriteLine("Nie możesz zostać senatorem");
+            }
+            
+            if (wiek >= 21 )
+            {
+                Console.WriteLine("Możesz zostać posłem");
+            }
+            else
+            {
+                Console.WriteLine("Nie możesz zostać posłem");
+            }
+            
+        
+        }
+
+    }
+
+}
